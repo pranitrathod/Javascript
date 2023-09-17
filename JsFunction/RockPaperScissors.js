27438197d4809123c136f1af0d53b5bc587d0d9a
pranitrathod <pranitrathodsocial@gmail.com>
2023-09-17 15:38:05 +0530

    Wrote Anonymours function using a rock paper scissors game!

--- JsFunction/RockPaperScissors.js
+++ JsFunction/RockPaperScissors.js
@@ -2,14 +2,27 @@ const ROCK='ROCK';
 const PAPER='PAPER';
 const SCISSORS='SCISSORS';
 
-const game=function ()
+const game=function () {return prompt(`${ROCK},${PAPER},${SCISSORS}`,'').toUpperCase();}
+
+
+const getComputerChoice=function ()
 {
-    return prompt(`${ROCK},${PAPER},${SCISSORS}`,'').toUpperCase();
+const randomValue=Math.random();
+if(randomValue<0.34) return ROCK;
+else if(randomValue<0.67) return PAPER;
+else return SCISSORS;
 }
 
-const choice=game();
-console.log(typeof choice);
-if(choice!== 'ROCK' && choice!== 'PAPER' && choice!== 'SCISSORS')
-{ console.log('INVALID');
+const whoWins=function()
+{
+if(comp===Yourchoice)return 'DRAW!!!';
+else if(comp===PAPER && Yourchoice===ROCK || comp === ROCK && Yourchoice===SCISSORS || comp===SCISSORS && Yourchoice===PAPER)return 'COMPUTER WON!!!';
+else return 'YOU WON!!!';
+}
+
+const Yourchoice=game();
+const comp=getComputerChoice();
+if(Yourchoice!== 'ROCK' && Yourchoice!== 'PAPER' && Yourchoice!== 'SCISSORS')
+{ alert(`${Yourchoice} is invalid choice!`);
 }else
-console.log(choice);
+console.log(`Computer Choose ${comp} & You choose ${Yourchoice} , ${whoWins()}`);
