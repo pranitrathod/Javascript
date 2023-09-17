7a0ca006f63d2dd776f17f25d032bb9d169e3c95
pranitrathod <pranitrathodsocial@gmail.com>
2023-09-17 17:06:58 +0530

    wrote a code for event Listener button element i.e start

--- JsFunction/RockPaperScissors.js
+++ JsFunction/RockPaperScissors.js
@@ -1,28 +1,31 @@
 const ROCK='ROCK';
 const PAPER='PAPER';
 const SCISSORS='SCISSORS';
-
+const winner=document.getElementById('Winner');
 const game=function () {return prompt(`${ROCK},${PAPER},${SCISSORS}`,'').toUpperCase();}
-
-
-const getComputerChoice= ()=>//used arrow function here for our understanding purpose!
+const startTheGame1=document.getElementById('start');
+function startTheGame()
 {
-const randomValue=Math.random();
-if(randomValue<0.34) return ROCK;
-else if(randomValue<0.67) return PAPER;
-else return SCISSORS;
+    const getComputerChoice = () =>//used arrow function here for our understanding purpose!
+    {
+        const randomValue = Math.random();
+        if (randomValue < 0.34) return ROCK;
+        else if (randomValue < 0.67) return PAPER;
+        else return SCISSORS;
+    }
+
+    const whoWins = function () {
+        if (comp === Yourchoice) return 'DRAW!!!';
+        else if (comp === PAPER && Yourchoice === ROCK || comp === ROCK && Yourchoice === SCISSORS || comp === SCISSORS && Yourchoice === PAPER) return 'COMPUTER WON 🎉';
+        else return 'YOU WON 🎉!!';
+    }
+
+    const Yourchoice = game();
+    const comp = getComputerChoice();
+    if (Yourchoice !== 'ROCK' && Yourchoice !== 'PAPER' && Yourchoice !== 'SCISSORS') {
+        alert(`${Yourchoice} is invalid choice!`);
+    } else
+        winner.innerHTML = `Computer Choose ${comp} & You choose ${Yourchoice} , ${whoWins()}`;
 }
 
-const whoWins=function()
-{
-if(comp===Yourchoice)return 'DRAW!!!';
-else if(comp===PAPER && Yourchoice===ROCK || comp === ROCK && Yourchoice===SCISSORS || comp===SCISSORS && Yourchoice===PAPER)return 'COMPUTER WON!!!';
-else return 'YOU WON!!!';
-}
-
-const Yourchoice=game();
-const comp=getComputerChoice();
-if(Yourchoice!== 'ROCK' && Yourchoice!== 'PAPER' && Yourchoice!== 'SCISSORS')
-{ alert(`${Yourchoice} is invalid choice!`);
-}else
-console.log(`Computer Choose ${comp} & You choose ${Yourchoice} , ${whoWins()}`);
+startTheGame1.addEventListener('click',startTheGame);
