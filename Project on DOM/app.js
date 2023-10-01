be0d1fa3188c42a4b96178b685270c4484259d86
pranitrathod <pranitrathodsocial@gmail.com>
2023-10-02 02:08:53 +0530

    DOM practice by build a small project!

--- Project on DOM/app.js
+++ Project on DOM/app.js
@@ -1,14 +1,28 @@
 const model =document.getElementById('add-modal');
 const buttons=model.lastElementChild;
-const cancleBtn=buttons.firstElementChild;
+const cancleMoviesBtn=buttons.firstElementChild;
+const addMoviesBtn=cancleMoviesBtn.nextElementSibling;
+const userInputs=document.querySelectorAll('input');
+
 
 // const addMovies=header.lastElementChild; this approch is not good for long run if any developer add new element,js will always select lastElement
 
 const addMovies=document.querySelector('header button');
 addMovies.addEventListener('click',()=>{
-    model.style.display='block';
+    model.classList.toggle('visible');
 })
-cancleBtn.addEventListener('click',()=>
-{
-    model.style.display='none';
+cancleMoviesBtn.addEventListener('click',()=>{
+    model.classList.toggle('visible');
+})
+
+addMoviesBtn.addEventListener('click',()=>{
+    const titleValue=userInputs[0].value;
+    const imageValue=userInputs[1].value;
+    const ratingValue=userInputs[2].value;
+
+    if(titleValue.trim() ==='' || imageValue.trim()==='' || ratingValue <1 || ratingValue>5)
+    {
+        alert('FUCK YOU ENTER VALID THINGS MOTHERFUCKER!!!!!!!!!!!!!!!!!!1');
+    }
+
 })
