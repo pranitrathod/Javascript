6d740e27df56dded21c97fefd77ae1610b5f23df
pranitrathod <pranitrathodsocial@gmail.com>
2023-10-03 18:15:18 +0530

    Learning Arrays!

---
+++ Arrays/app.js
@@ -0,0 +1,15 @@
+//There are few ways we can declare array
+
+//1.
+const arr1=[1,2,3,4,5,6];
+console.log(arr1);
+
+//2.
+//Here we can either write new or not it doesnt matter even if you try out Array() it will work!
+const arr2=Array(5);//first parameter state length and if you specify more then 1 number it will count as [1,2]
+console.log(arr2);
+
+//3.
+const arr3=Array.of(1,2,34,4);//this is another method we can use to declare an array!
+console.log(arr3);
+
