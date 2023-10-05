3d60406714a42de0e9072de0eefc55999001b44a
pranitrathod <pranitrathodsocial@gmail.com>
2023-10-05 16:42:02 +0530

    Maps,Reduce,Destucturing etc

--- Arrays/ArraysFunctions.js
+++ Arrays/ArraysFunctions.js
@@ -42,8 +42,55 @@
 //     console.log('Not found');
 // }
 
-const arr=[1,2,3,4,5,6,7,8];
-const newObj=arr.filter((num,id,arr)=>{return num>6;});console.log(newObj);
+// const arr=[1,2,3,4,5,6,7,8];
+// const newObj=arr.filter((num,id,arr)=>{return num>6;});console.log(newObj);
+//
 
+// const arr=[1,2,3,4,4,5];
+//
+// const transformation=arr.map((no,id,arr)=>{
+//     const transform={no:id};
+//     return transform;
+//
+// })
+// console.log(transformation);
+
+//Reduce()-its nothing it's basically a function which does a job of added a variable in background without we declaring in
+//its fast and doesnt require loops like we used to use in other languages,in above function we dont need loop thats why we use maps,filter,etc
+
+// const name=['P','R','A','N','I','T'];
+// console.log(name.reduce((prevChar,currChar)=>prevChar+currChar,''));//much cleaner
+//
+
+
+//Spread operator-This we have seen in function parameter were we can able to throw as many parameter as you wish in function
+//but in Javascript we have something special for array to lets see how
+
+// const arr=[1,2,3,4,5];
+// const copiedArr=[...arr];//with any loop we can able to copy as many arrList we can but this will be brand new array whatever happens to arr in future
+// //does not reflect on copiedArr just go through below code!
+// arr.push(6);
+// // console.log(arr,copiedArr);
+//
+// //Another best example here is min() method
+//
+// console.log(Math.min(arr));//returns => NaN
+//
+// // here can use ... operator instead for making thos arr to iterable sperable numbers
+//
+// console.log(Math.min(...arr));
+
+
+//Array's Destructuring
+
+//
+// const obj=[{name:'PRANIT',age:30,file:20},{name:'Pratik',age:31}];
+// const [name,...rest]=obj;
+// console.log(name.name,...rest);
+
+//
+// const differentArray=['PRANIT','RATHOD','Mr.','SOFTWARE ENGINEER',24];
+// const [firstName,LastName,...rest]=differentArray;//This is how we do destructing in Javascript on arrays;
+// console.log(firstName,LastNamerest);
 
 
