47d9b007c7f43aba41845529b95d1c3f84e72cbd
pranitrathod <pranitrathodsocial@gmail.com>
2023-10-11 15:21:37 +0530

    Two ways to copy objects

---
+++ Object/copyObject.js
@@ -0,0 +1,11 @@
+//There are two methods we can copy the object using ...spread operator and assign()
+
+//1.
+const object={books:'Atomic habits'}
+const object2={...object};
+object2.name='James';
+console.log(object2," ",object);
+
+//2.
+const object3=Object.assign({name:'PRANIT',age:23},object);
+console.log(object3);
