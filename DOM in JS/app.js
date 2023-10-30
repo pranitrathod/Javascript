ab37cc00f27b358c11fab4d9338b41c30b712977
pranitrathod <pranitrathodsocial@gmail.com>
2023-10-30 21:10:01 +0530

    Practice on events

--- DOM in JS/app.js
+++ DOM in JS/app.js
@@ -4,11 +4,9 @@
 const ul=document.querySelector('ul');
 console.log(ul);
 
-ul.innerHTML=ul.innerHTML +'<li>item 4</li>'//this all are brand new element
+ul.innerHTML=ul.innerHTML +'<li>item 4</li>';//this all are brand new element
 
 ul.insertAdjacentHTML('beforeend','<li>item 4</li>'); //This is only add element which we want rather then adding all new element
-
-
 
 
 
---
+++ Events/app.js
@@ -0,0 +1,11 @@
+const btn=document.querySelector('button');
+function msg(e)
+{   console.log(e);
+    alert('DONE!',e);
+}
+const msg1=msg.bind(this);
+btn.addEventListener('click',msg1);
+setTimeout(()=>{
+    btn.removeEventListener('click',msg1);
+    console.log('over!');
+    },5000);
