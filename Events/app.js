42e3b602dfb9a5f726c4162a54d95b2417949d32
pranitrathod <pranitrathodsocial@gmail.com>
2023-11-03 18:23:04 +0530

    Brief understanding of closest event delegation!

--- Events/app.js
+++ Events/app.js
@@ -1,22 +1,28 @@
 
 const ul=document.getElementById('2');
-const ul2=document.querySelectorAll('input');
+
 ul.addEventListener('click',e=>{
-    console.log(e);
+    // console.log(e);
     e.target.closest('li').classList.toggle('highlist');
     // e.target.closest('li').classList.toggle('highlist');
 })
-
-// ul2.addEventListener('click',e=>{
-//     console.log(e);
-//     e.target.closest('li').classList.toggle('checks');
-//     // e.target.closest('li').classList.toggle('highlist');
-// })
-// check.addEventListener('click',e=>{e.target})
-
-
-
-ul2.forEach(e=>{
-  console.log(e);
+const ul2=document.getElementById('1');
+ul2.addEventListener('click',e=>{
+    console.log(e.target.closest('ul'));
+    e.target.closest('label').classList.toggle('checks');
+    // e.target.closest('li').classList.toggle('highlist');
 })
+
+
+
+//Every good example why does input doesn't directly work
+// we have to wrap into some element individually because ul2 doesnt know which input it trigger
+//we have to put inside the structure element such as label or li
+//then it will understand its l1-1 input or l1-2 input so on and so forth
+
+//It does work like this div->ul->li thats all for only list or other kinds of stuff
+//It doesnt work for input we have to add a label or other element to get the it
+
+
+
 
