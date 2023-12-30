abe569788f4e768cc949254cd6c97a3a5876ed1e
pranitrathod <pranitrathodsocial@gmail.com>
2023-12-30 17:02:10 +0530

    Learned and practices Promises and async-await!

--- Async-concept/Script.js
+++ Async-concept/Script.js
@@ -1,40 +1,54 @@
 const but=document.querySelector("button");
 console.log(but);
-function getLocation()
+async function getLocation()
 {
     navigator.geolocation.getCurrentPosition(
         position => {console.log(position)},
           error=>{console.log(error);})
-
     //This above part is taken cared by browser
     //And when the below code is vanished or executed from the stack ,message queue will put the above code to the stack by
      //event loop concept-->This is async-functions!
     //and here navigator is browser API
-
     console.log("I ran faster then you,Congratulations you learned Async-concept!");
 }
-
 but.addEventListener('click',getLocation);
-
-
 //Convert this code into promises
-
 function something(){
     return 20;
 }
+// async function a(){}
+// const getAnswer= (()=>{
+//     return new Promise((resolve,reject)=>{
+//         try{
+//             resolve(getLocation());
+//         }
+//         catch(error){
+//             reject(error);}
+//     })
+// })
+// async function getPromise(){ const ans=await getAnswer();
+//     console.log("learning async");
+// console.log(ans);}
+// getPromise();
 
 
-const getAnswer=(()=>{
+async function setTimer(number)
+{
     return new Promise((resolve,reject)=>{
-        try{
-            resolve(getLocation());
-        }
-        catch(error){
-            reject(error);}
-    })
+        console.log("downloading!!!!!");
+       setTimeout(()=>{
+
+           setTimeout(()=>{resolve(getLocation())},1);
+           }
+           ,number);
 })
+}
+const ans=setTimer(100);
 
-const getPromise=getAnswer().then(result=>{console.log(result)}).catch(error=>{console.log(error)});
+// setTimer(1000).then((result)=>console.log(result));
+// const getPromises=getAnswer().then((result)=>{return result});
+// console.log(getPromises);
 
-
+    // .then(result=>{console.log(result)})
+    // .catch(error=>{console.log(error)});
 
