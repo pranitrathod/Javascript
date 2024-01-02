0a9456486a102ac1789355b52757092a5d4d79f9
pranitrathod <pranitrathodsocial@gmail.com>
2024-01-02 16:15:06 +0530

    Practiced on promisified!

--- Https-req-res/script.js
+++ Https-req-res/script.js
@@ -1,8 +1,29 @@
-const xhr=new XMLHttpRequest();
-xhr.open('GET',"https://jsonplaceholder.typicode.com/posts");
+const postList=document.querySelector(".posts");
+const postDesc=document.getElementById("desc");
 
-xhr.onload=function(){
-    const gotData=JSON.parse(xhr.response);
-    console.log(gotData);
+const xhr=new XMLHttpRequest();
+function sendHTTP(method,url){
+
+    const promise=new Promise((resolve,reject)=>{
+        xhr.open(method,url);
+        xhr.onload=function(){
+            const gotData=JSON.parse(xhr.response);
+            resolve(gotData);
+
+        }
+        xhr.send();
+    });
+    return promise;
 }
-xhr.send();
+
+ async function fetchData(){
+ const gotData=await sendHTTP('GET',"https://jsonplaceholder.typicode.com/posts");
+     for (let post of gotData) {
+         const postTemp = document.importNode(postDesc.content, true);
+         postTemp.querySelector("h2").textContent = post.title;
+         postTemp.querySelector("p").textContent = post.body;
+         postList.append(postTemp);
+     };
+
+}
+fetchData();
